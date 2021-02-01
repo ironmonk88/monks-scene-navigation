175071e28898343cf6c0c9a78661abe1299e6062
ironmonk88 <75920956+ironmonk88@users.noreply.github.com>
2021-01-31 23:31:46 -0800

    Add files via upload

---
+++ monks-scene-navigation.js
@@ -0,0 +1,51 @@
+﻿import { registerSettings } from "./settings.js";
+
+export let debug = (...args) => {
+    if (debugEnabled > 1) console.log("DEBUG: monks-scene-navigation | ", ...args);
+};
+export let log = (...args) => console.log("monks-scene-navigation | ", ...args);
+export let warn = (...args) => {
+    if (debugEnabled > 0) console.warn("monks-scene-navigation | ", ...args);
+};
+export let error = (...args) => console.error("monks-scene-navigation | ", ...args);
+export let i18n = key => {
+    return game.i18n.localize(key);
+};
+
+export class MonksSceneNavigation {
+    static init() {
+	    log("initializing");
+        // element statics
+       // CONFIG.debug.hooks = true;
+
+        
+        registerSettings();
+    }
+
+    static ready() {
+    }
+
+    
+}
+
+/**
+ * Assorted hooks
+ */
+/* ------------------------------------ */
+/* Initialize module					*/
+/* ------------------------------------ */
+Hooks.once('init', async function () {
+    log('Initializing Monks Little Details');
+    MonksLittleDetails.init();
+});
+
+/**
+ * Ready hook
+ */
+Hooks.on("ready", MonksLittleDetails.ready);
+
+Hooks.on("renderSceneNavigation", (app, html, data) => {
+    $(html).addClass('monks-scene-navigation');
+    log('render scene navigation', data);
+});
+
